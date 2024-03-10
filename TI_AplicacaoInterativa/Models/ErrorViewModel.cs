2e8075be0be13f0e5af0e108cbd67489473f9fd4
Davidsb04 <caisilvasb756@hotmail.com>
2024-03-10 15:38:02 -0300

    Solução de ConflitosAtualização de Banco de Dados

---
+++ TI_AplicacaoInterativa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TI_AplicacaoInterativa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
